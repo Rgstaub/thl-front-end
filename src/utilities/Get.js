cf5cc1da9e88f3f858700288eaf9fe8318d8180d
Rgstaub <rgstaub@gmail.com>
2018-07-04 14:15:22 -0400

    heroku

--- src/utilities/Get.js
+++ src/utilities/Get.js
@@ -9,7 +9,7 @@ const Get = (url) => {
       'content-type': 'application/json'
     },
     method: 'GET', // *GET, POST, PUT, DELETE, etc.
-    mode: 'no-cors', // no-cors, cors, *same-origin
+    mode: 'cors', // no-cors, cors, *same-origin
     redirect: 'follow', // manual, *follow, error
     referrer: 'no-referrer', // *client, no-referrer
   }).then(response => {
