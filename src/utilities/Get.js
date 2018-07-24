ba75a4cdfb245e2f605d5f1a66b7fd4dc27041bd
Rgstaub <rgstaub@gmail.com>
2018-07-23 22:48:08 -0400

    heroku

--- src/utilities/Get.js
+++ src/utilities/Get.js
@@ -3,8 +3,8 @@ const Get = (url) => {
   return fetch(`http://localhost:3001/${url}`, {
     //body: JSON.stringify(data), // must match 'Content-Type' header
     cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: 'include', // include, same-origin, *omit
-    headers: {},
+    //credentials: 'include', // include, same-origin, *omit
+
     method: 'GET', // *GET, POST, PUT, DELETE, etc.
     mode: 'cors', // no-cors, cors, *same-origin
     redirect: 'follow', // manual, *follow, error
