827c7d147dfbd988986c9eb527617b7e53c99a6b
Rgstaub <rgstaub@gmail.com>
2018-07-23 23:08:19 -0400

    heroku

--- src/utilities/Get.js
+++ src/utilities/Get.js
@@ -2,13 +2,13 @@ const Get = (url) => {
 
   return fetch(`https://thl-back-end.herokuapp.com/${url}`, {
     //body: JSON.stringify(data), // must match 'Content-Type' header
-    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    //cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
     //credentials: 'include', // include, same-origin, *omit
 
     method: 'GET', // *GET, POST, PUT, DELETE, etc.
     mode: 'cors', // no-cors, cors, *same-origin
-    redirect: 'follow', // manual, *follow, error
-    referrer: 'no-referrer', // *client, no-referrer
+    // redirect: 'follow', // manual, *follow, error
+    // referrer: 'no-referrer', // *client, no-referrer
   }).then(response => {
     if (response.status === 200) {
       return response.json()
